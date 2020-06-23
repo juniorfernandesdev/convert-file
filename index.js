fe0f5d45c55963b998021a05154639e720d3bb5c
Junior Fernandes <juniorbass8@gmail.com>
2020-06-23 14:59:12 -0300

    Convertendo um arquivo XLSX em JSON

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var xlsx =  require("xlsx");
+
+var wb = xlsx.readFile('./depoimentos.xlsx');
+
+var fs = require('fs');
+var ws = wb.Sheets["depoimentos"];
+
+var data = xlsx.utils.sheet_to_json(ws);
+
+var newData = data.map(function(record) {
+    record.endereco_foto = record.aluno;
+    record.depoimento_site_new = record.depoimento_site.split('\r\n'); 
+    delete record.depoimento_completo;
+    delete record.depoimento_site;
+    return record;
+});
+
+console.log(newData);
+
+const depoimento = JSON.stringify(newData);
+
+fs.writeFile('depoimentos.json', depoimento, (err) => {
+    if(err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
+
